c176efd79ffb47eec360e5aa689dc47c6bdc303e
CarlosPerezJuarez <carlos.perezjuarez@vasscompany.com>
2026-03-09 09:47:14 +0100

    fix hello block

--- blocks/hello/hello.js
+++ blocks/hello/hello.js
@@ -1,5 +1,5 @@
 export default function decorate(block) {
-  const message = document.createElement("p");
-  message.textContent = "Hello from eds-ue-test!";
+  const message = document.createElement('p');
+  message.textContent = 'Hello from eds-ue-test!';
   block.append(message);
 }
